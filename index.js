f7cda4cd95fe5b573b289be800d9a8b00f124b4a
freelancersifat627e <freelancersifat627@gmail.comm>
2024-02-02 21:37:07 +0600

    some code updated

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 // middleware
 app.use(cors());
@@ -35,11 +35,30 @@ async function run() {
 
 
     app.get('/products' , async(req,res) => {
-        const result = await productCollection.find().toArray();
+        console.log(req.query);
+        const page = parseInt(req.query.page) || 0;
+        const limit = parseInt(req.query.limit) || 10;
+        const skip = page * limit;
+        const result = await productCollection.find().skip(skip).limit(limit).toArray();
         res.send(result);
     });
 
+    app.get('/totalProducts' , async(req,res) => {
+        
+        const result = await productCollection.estimatedDocumentCount();
+        console.log(result)
+        // res.send({ result});
+        res.send({result});
+    });
 
+    app.post('/productsByIds' , async(req,res) => {
+        const ids = req.body;
+        const objectIds = ids?.map(id => new ObjectId(id))
+        const query = {_id : {$in : objectIds}}
+        console.log(ids);
+        const result = await productCollection.find(query).toArray();
+        res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
